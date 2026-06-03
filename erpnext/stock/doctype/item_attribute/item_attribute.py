819b9b8fc78fdac8672f8c774d5b86b1a0efd019
Loïc Oberle <loic@dokos.io>
2026-06-03 23:02:12 +0200

    refactor(item_attribute): replace sql with qb in validate_existing_items
    Replace SQL with query builder to ensure compatibility with postgres
    
    Contribution made on behalf of Orange SA

--- erpnext/stock/doctype/item_attribute/item_attribute.py
+++ erpnext/stock/doctype/item_attribute/item_attribute.py
@@ -63,19 +63,19 @@ class ItemAttribute(Document):
 		attributes_list = [d.attribute_value for d in self.item_attribute_values]
 
 		# Get Item Variant Attribute details of variant items
-		items = frappe.db.sql(
-			"""
-			select
-				i.name, iva.attribute_value as value
-			from
-				`tabItem Variant Attribute` iva, `tabItem` i
-			where
-				iva.attribute = %(attribute)s
-				and iva.parent = i.name and
-				i.variant_of is not null and i.variant_of != ''""",
-			{"attribute": self.name},
-			as_dict=1,
-		)
+		ItemVariantAttribute = frappe.qb.DocType("Item Variant Attribute")
+		Item = frappe.qb.DocType("Item")
+		items = (
+			frappe.qb.from_(ItemVariantAttribute)
+			.join(Item)
+			.on(ItemVariantAttribute.parent == Item.name)
+			.select(Item.name, ItemVariantAttribute.attribute_value.as_("value"))
+			.where(
+				(ItemVariantAttribute.attribute == self.name)
+				& (Item.variant_of.isnotnull())
+				& (Item.variant_of != "")
+			)
+		).run(as_dict=1)
 
 		for item in items:
 			if self.numeric_values:
